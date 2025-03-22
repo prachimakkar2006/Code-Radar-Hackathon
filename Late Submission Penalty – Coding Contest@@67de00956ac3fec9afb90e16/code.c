36a3b05f555a3b1f992b4452ab88e150da1ef39f
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-03-22 14:00:56 +0530

    Solution to Late Submission Penalty – Coding Contest

---
+++ Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int calculatePenalty(int subDay,int subMonth,int subYear,int dueDay;int dueMonth;int dueYear){
+    int pay;
+    if(subMonth==dueMonth && subYear==dueYear && subDay!=dueDay){
+        pay=10*(subDay-dueDay);
+    }
+    else if(subYear==dueYear){
+        pay=200*(subMonth-dueMonth);
+    }
+    else{
+        pay=5000;
+    }
+    return pay;
+}
